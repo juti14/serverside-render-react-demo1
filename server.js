08e41ef2bb04b86b84f308297c99feede7cce9a2
Juti Buican <juti@fsbtech.com>
2021-11-17 15:28:43 +0000

    style component on serverside using ServerStyleSheet

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
+import { ServerStyleSheet } from 'styled-components';
 import path from 'path';
 import fs from 'fs';
 import App from './src/App';
@@ -11,10 +12,12 @@ const app = express();
 app.use(express.static('./build', { index: false }))
 
 app.get('/*', (req, res) => {
+  const sheet = new ServerStyleSheet();
   const reactApp = renderToString(
-    <StaticRouter location={req.url}>
-      <App />
-    </StaticRouter>
+    sheet.collectStyles(
+      <StaticRouter location={req.url}>
+        <App />
+      </StaticRouter>)
   );
 
   const templateFile = path.resolve('./build/index.html');
@@ -25,6 +28,7 @@ app.get('/*', (req, res) => {
 
     return res.send(
       data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+        .replace('{{ styles }}', sheet.getStyleTags())
     )
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import styled from 'styled-components'
+
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Articles } from './pages/Articles';
 
+const HeadingStyle = styled.h1`
+	color: #22629b;
+	font-size: 50px; 
+`
+
 const App = () => {
 	return (
 		<>
-			<h1>Server-Side Rendering Example</h1>
+			<HeadingStyle>Server-Side Rendering Example</HeadingStyle>
 			<ul>
 				<li>
 					<Link to="/">Home</Link>
